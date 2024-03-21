8370d355bb72237893b2030dfe83fb2a878489fb
ArunaluB <it22515612@my.sliit.lk>
2024-03-21 17:19:20 +0530

    httpcode add

--- src/modules/product/productcontroller.controller.ts
+++ src/modules/product/productcontroller.controller.ts
@@ -1,20 +1,17 @@
 
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { ProductServiseService } from './productservise.service';
+import { HttpStatus } from '@nestjs/common';
 // genaral controler path set
 @Controller('product')
 export class ProductcontrollerController {
   // Dependency Injection is used here nestjs
-  constructor(private ProductServiseService : ProductServiseService) {}
-
-  @Get('/')
-  getAllproduct() {
-    return this.ProductServiseService.getAllproduct();
-  }
+  // ...
 
   @Post('/')
-  listaddproduct(@Body()product) {
-    return {data:product};
+  @HttpCode(HttpStatus.OK)
+  listaddproduct(@Body() product) {
+    return { data: product };
   }
 
 }
